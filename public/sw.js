ef2b3ee67c2f6226b2766a6d031be04bb576c8c4
Mertcan Sevigen <mertcansevigen@MacBookPro.hgw.local>
2026-03-02 00:40:21 +0300

    add fetch handler for pwa

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,11 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   self.skipWaiting()
 })
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(clients.claim())
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request))
 })
